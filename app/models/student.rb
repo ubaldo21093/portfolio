5aa039040fb481af2166ea631149a9c2c055b0da
ubaldo21093 <ubogarin@msudenver.edu>
2024-10-20 19:02:06 -0600

    m03 completed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,32 @@
 class Student < ApplicationRecord
+    
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+   validates :major, presence: true
+   validates :school_email, presence: true
+   validates :school_email, uniqueness: true
+   validate :school_email_format
+   has_one_attached :profile_picture
+   validate :acceptable_image
+
+   def school_email_format
+       unless school_email =~ /\A[\w+\-.]+@msudenver\.edu\z/i
+           errors.add(:school_email, "must be an @msudenver.edu email address")
+       end
+   end
+
+   def acceptable_image
+       return unless profile_picture.attached?
+
+       unless profile_picture.blob.byte_size <= 1.megabyte
+           errors.add(:profile_picture, "is too big")
+       end
+
+       acceptable_types = ["image/jpeg", "image/png"]
+       unless acceptable_types.include?(profile_picture.content_type)
+           errors.add(:profile_picture, "must be a JPEG or PNG")
+       end
+
+ end
+
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
-
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "should raise error when saving student without first name" do
+assert_raises ActiveRecord::RecordInvalid do
+Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major:
+"CS")
+end
+end
+test "should not allow duplicate school_email" do
+# This retrieves the student with the key 'one' from students.yml
+student = students(:one)
+assert_raises ActiveRecord::RecordInvalid do
+Student.create!(first_name: "Run", last_name: "Time", school_email:
+"rtime@msudenver.edu", major: "CSI")
+end
+end
+test "should save student with valid attributes" do
+student = Student.create!(first_name: "Ruby", last_name: "Rails", school_email:
+"rrails@msudenver.edu", major: "CS")
+assert student.persisted?, "Student was not persisted"
+End
 end
